da3e04ef833e28391c89737a7cdbd97fff3ea4d9
Wang Pengfei <wangpengfei@ymt360.com>
2015-07-02 19:57:27 +0800

    配置好更改自动刷新

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var elixir = require('laravel-elixir');
 require('laravel-elixir-angular');
+require('laravel-elixir-livereload');
+
 elixir(function(mix) {
     mix.sass('app.scss');
     mix.scripts(['angular.min.js', 'script.js']);
+    mix.livereload();
 });
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,44 +2,19 @@
 <html>
     <head>
         <title>Laravel</title>
-
-        <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
+        <link rel="stylesheet" href="css/app.css">
+        <script type="text/javascript" src="js/all.js"></script>
+        @if ( Config::get('app.debug') )
+        <script type="text/javascript">
+            document.write('<script src="//localhost:35729/livereload.js?snipver=1" type="text/javascript"><\/script>');
+            </script> 
+            @endif 
+        </head>
+        <body>
+            <div class="container">
+                <div class="content">
+                    <div class='bg'>                   wangpengfed</div>
+                </div>
             </div>
-        </div>
-    </body>
-</html>
+        </body>
+    </html>
